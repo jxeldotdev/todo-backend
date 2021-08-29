e5d6b5ef8a741a511dadc207502e5c8e87df77cc
Joel Freeman <joel@jxel.dev>
2021-08-29 14:34:51 +1200

    Added Alembic migrations again

---
+++ src/alembic/versions/4ee595c9eacd_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 4ee595c9eacd
+Revises: 
+Create Date: 2021-08-29 14:31:45.776135
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4ee595c9eacd'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
